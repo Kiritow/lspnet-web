c50832b4cbc7be9a358f6ebdf05beb675a393f05
Kiritow <1362050620@qq.com>
2025-05-05 07:07:11 +0000

    hide menu

--- src/menu.ts
+++ src/menu.ts
@@ -1,6 +1,6 @@
 import { Star } from '@element-plus/icons-vue'
 export const sidearMenu = [
   { index: '/', title: 'Clusters', icon: Star, display: true },
-  { index: '/nodes', title: 'Node List', icon: Star, display: true },
-  { index: '/links', title: 'Link List', icon: Star, display: true },
+  { index: '/nodes', title: 'Node List', icon: Star, display: false },
+  { index: '/links', title: 'Link List', icon: Star, display: false },
 ]
